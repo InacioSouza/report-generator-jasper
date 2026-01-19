e3b2a7710c9d3eb0446f99e5c1b36ad969c90c13
Inácio Souza Rocha <inacio.rocha@sonner.com.br>
2026-01-19 09:43:12 -0300

    Correção: Importações para classes inexistentes removidas

--- src/main/java/br/com/report_generator/repository/VersaoRelatorioRepository.java
+++ src/main/java/br/com/report_generator/repository/VersaoRelatorioRepository.java
@@ -2,11 +2,9 @@ package br.com.report_generator.repository;
 
 import br.com.report_generator.model.VersaoRelatorio;
 import br.com.report_generator.repository.generic.GenericRepository;
-import br.com.report_generator.shared.Teste;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
