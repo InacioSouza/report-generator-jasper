403a39e96456bd6ccb398a8cac3ca4e27549dc1d
Inácio Souza Rocha <inaciosouzarocha69@gmail.com>
2026-03-24 16:04:47 -0300

    refactor: Configuração do testcontainers ajustada
     Vínculo do testcontainers com Spring criado

--- src/test/java/br/com/report_generator/contexto/IntegrationTestContext.java
+++ src/test/java/br/com/report_generator/contexto/IntegrationTestContext.java
@@ -1,6 +1,8 @@
 package br.com.report_generator.contexto;
 
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.postgresql.PostgreSQLContainer;
@@ -13,8 +15,11 @@ public abstract class IntegrationTestContext {
     static PostgreSQLContainer postgres =
             new PostgreSQLContainer("postgres:15");
 
-    static {
-        postgres.start();
+    @DynamicPropertySource
+    static void configureProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgres::getJdbcUrl);
+        registry.add("spring.datasource.username", postgres::getUsername);
+        registry.add("spring.datasource.password", postgres::getPassword);
     }
 
 }
